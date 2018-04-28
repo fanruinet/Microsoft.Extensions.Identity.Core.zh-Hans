9f4d276ea316b04ef194585a41722f5714436ced
fanruinet <albert@fanrui.net>
2018-04-28 21:23:45 +0800

    Initial release based on Microsoft.Extensions.Identity.Core v2.2.0.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Microsoft.Extensions.Identity.Core.zh-Hans
 The Simplified Chinese translation resource for the Microsoft.Extensions.Identity.Core package.
+
+To install this package from NuGet:
+Install-Package SportsLoop.Extensions.Identity.Core.zh-Hans -Version 2.2.0
+
---
+++ buildres.cmd
@@ -0,0 +1,8 @@
+SET PATH=%PATH%;C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.7.1 Tools;
+SET OUTPUTPATH=".\nuget\lib\netstandard2.0\zh-Hans"
+IF NOT EXIST %OUTPUTPATH% mkdir %OUTPUTPATH%
+
+resgen Microsoft.Extensions.Identity.Core.Resources.zh-Hans.resx
+al -target:lib -embed:Microsoft.Extensions.Identity.Core.Resources.zh-Hans.resources -culture:zh-Hans -out:%OUTPUTPATH%\Microsoft.Extensions.Identity.Core.resources.dll -keyfile:Key.snk /productv:2.2.0 /v:2.2.0.0 /title:Microsoft.Extensions.Identity.Core
+
+nuget pack nuget\Package.nuspec
